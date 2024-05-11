fa68db295efa31e1686dc0083f5251a442a06fd9
Justin Smith <jmsjustin@gmail.com>
2024-05-11 14:35:08 -0400

    reverted some old issues and fixed some minor ones

--- src/ExpensesShow.jsx
+++ src/ExpensesShow.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
-export function ExpensesShow(props) {
-  return (
-    <div>
-      <h1>Expense information</h1>
-      <p>Name: {props.expense.name}</p>
-      <p>Amount: {props.expense.amount}</p>
-    </div>
-  );
-}
+// export function ExpensesShow(props) {
+//   return (
+//     <div>
+//       <h1>Expense information</h1>
+//       <p>Name: {props.expense.name}</p>
+//       <p>Amount: {props.expense.amount}</p>
+//     </div>
+//   );
+// }
